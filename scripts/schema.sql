27fbd15c88f1708aac76beeda9e3e42c1b5a4028
jtuvek <132848981+jtuvek@users.noreply.github.com>
2023-11-12 16:33:13 -0500

    fixed schema

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-  id INT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10, 2) NOT NULL,
   department_id INT,
@@ -25,11 +25,11 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-  id INT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
-  manager_id INT,
+  department_id INT,
   FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
